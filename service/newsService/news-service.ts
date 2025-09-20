15ee53a02a9c664f095eb43aad7a2e3e04ed4efa
Theusma26 <matheusrodriguesoliveira258@gmail.com>
2025-09-20 02:12:35 -0300

    chore: adicionando campos de data e fonte

--- service/newsService/news-service.ts
+++ service/newsService/news-service.ts
@@ -2,7 +2,7 @@ import api from "../baseService/api";
 
 export async function getNewsByQuery(query: string, page = 1, pageSize = 10) {
     const response = await api.get(
-        `/everything?q=${query}&page=${page}&pageSize=${pageSize}&apiKey=aa107396da7c429a8a24c371b6dcf41a`
+        `/everything?q=${query}&page=${page}&pageSize=${pageSize}&sortBy=publishedAt&apiKey=aa107396da7c429a8a24c371b6dcf41a`
     );
     return {
         ...response.data,
