030f9468bd625cbbc3d030111f58856264ce5152
Theusma26 <matheusrodriguesoliveira258@gmail.com>
2025-09-19 21:47:03 -0300

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>
+        Lista de noticias
+      </Text>
+    </View>
+  );
+}
+
+
