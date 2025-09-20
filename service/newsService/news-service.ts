f866b489d16b36bdc8d39d99242529f08cbc9ff7
Theusma26 <matheusrodriguesoliveira258@gmail.com>
2025-09-20 16:23:44 -0300

    fix: adicionando variavel de ambiente

--- service/newsService/news-service.ts
+++ service/newsService/news-service.ts
@@ -2,7 +2,16 @@ import api from "../baseService/api";
 
 export async function getNewsByQuery(query: string, page = 1, pageSize = 10) {
     const response = await api.get(
-        `/everything?q=${query}&page=${page}&pageSize=${pageSize}&sortBy=publishedAt&apiKey=6efde79322414e209950be02da6ddbf9`
+        `/everything?q=${query}&page=${page}&pageSize=${pageSize}&sortBy=publishedAt&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
+    );
+    return {
+        ...response.data,
+        page,
+    };
+}
+export async function getNewsByCategory(query: string, page = 1, pageSize = 10) {
+    const response = await api.get(
+        `/top-headlines?category=${query}&page=${page}&pageSize=${pageSize}&sortBy=publishedAt&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
     );
     return {
         ...response.data,
